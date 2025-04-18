877ad4ad4ae758aa56cf4449832b7c160e016047
KIRUTHIKA S <89388434+Kiruthika162003@users.noreply.github.com>
2025-04-18 01:41:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import time
 
 # Load Hugging Face Token securely
 HF_TOKEN = st.secrets["HF_TOKEN"]
-model_id = model_id = "google/gemma-2b-it"
+model_id = model_id = "google/gemma-1.1-2b-it"
 st.set_page_config(page_title="Prince vs Dragon: Rescue Mastermind", layout="wide")
 
 st.markdown("""
